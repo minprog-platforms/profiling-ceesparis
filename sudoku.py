3992dbe970f84d81bf7d3e4ffb30895d46b9cb63
Cees Paris <ceesparis@wcw-stud-145-109-45-124.wireless.uva.nl>
2021-11-24 10:54:12 +0100

    flake8'ed

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Iterable, Sequence
+from typing import Iterable
 
 
 class Sudoku:
@@ -10,7 +10,7 @@ class Sudoku:
         self._grid: dict[tuple, int] = {}
         self.row_list: list[list: int] = []
         self.column_list: list[list: int] = []
-        # self.block_list: list[list: int] = []
+        self.block_list: list[list: int] = []
         y = 0
         x = 0
         for puzzle_row in puzzle:
@@ -31,40 +31,40 @@ class Sudoku:
                 sing_col.append(val_col)
             self.column_list.append(sing_col)
 
-        # my thing
-        # block_row_list = []
-        # for i in range(9):
-        #     x_start = (i % 3) * 3
-        #     y_start = (i // 3) * 3
+        block_row_list = []
+        for i in range(9):
+            x_start = (i % 3) * 3
+            y_start = (i // 3) * 3
 
-        #     for y in range(y_start, y_start + 3):
-        #         sing_block_row = []
-        #         for x in range(x_start, x_start + 3):
-        #             sing_block_row.append(self._grid[x, y])
-        #         block_row_list.append(sing_block_row)
-        
-        # for i in range(0, 27, 3):
-        #     whole_block = block_row_list[i] + block_row_list[i+1] + block_row_list[i+2]
-        #     self.block_list.append(whole_block)
-        # print(self.block_list)
-  
+            for y in range(y_start, y_start + 3):
+                sing_block_row = []
+                for x in range(x_start, x_start + 3):
+                    sing_block_row.append(self._grid[x, y])
+                block_row_list.append(sing_block_row)
 
-        
+        for i in range(0, 27, 3):
+            whole_block = block_row_list[i] + block_row_list[i+1] + block_row_list[i+2]
+            self.block_list.append(whole_block)
 
     def place(self, value: int, x: int, y: int) -> None:
         """Place value at x,y."""
         self._grid[x, y] = value
         self.row_list[y][x] = value
         self.column_list[x][y] = value
-        # self.block_list[x][y] = value
+        block_num = (y // 3) * 3 + x // 3
+        rel_block = self.block_list[block_num]
+        index_num = (y % 3) * 3 + x % 3
+        rel_block[index_num] = value
 
     def unplace(self, x: int, y: int) -> None:
         """Remove (unplace) a number at x,y."""
         self._grid[x, y] = 0
         self.row_list[y][x] = 0
         self.column_list[x][y] = 0
-        # self.block_list[x][y] = 0
-
+        block_num = (y // 3) * 3 + x // 3
+        rel_block = self.block_list[block_num]
+        index_number = (y % 3) * 3 + x % 3
+        rel_block[index_number] = 0
 
     def value_at(self, x: int, y: int) -> int:
         """Returns the value at x,y."""
@@ -73,25 +73,18 @@ class Sudoku:
 
     def options_at(self, x: int, y: int) -> Iterable[int]:
         """Returns all possible values (options) at x,y."""
-        options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        options = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-        # Remove all values from the row
-        for value in self.row_values(y):
-            if value in options:
-                options.remove(value)
-
-        # Remove all values from the column
-        for value in self.column_values(x):
-            if value in options:
-                options.remove(value)
+        values_row = set(self.row_values(y))
+        options = options - values_row
+        columns_row = set(self.column_values(x))
+        options = options - columns_row
 
         # Get the index of the block based from x,y
         block_index = (y // 3) * 3 + x // 3
 
-        # Remove all values from the block
-        for value in self.block_values(block_index):
-            if value in options:
-                options.remove(value)
+        block_row = set(self.block_values(block_index))
+        options = options - block_row
 
         return options
 
@@ -103,41 +96,21 @@ class Sudoku:
         for y in range(9):
             for x in range(9):
                 if self._grid[x, y] == 0:
-                    return x, y 
-        x, y = -1, -1 
+                    return x, y
+        x, y = -1, -1
         return x, y
-
 
     def row_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th row."""
-        # values = []
 
-        # for j in range(9):
-        #     values.append(self._grid[j, i])
-
-        # return values
-
-        # my thing
         values = self.row_list[i]
-
         return values
-
-
 
     def column_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th column."""
-        # values = []
-
-        # for j in range(9):
-        #     # values.append(self.value_at(i, j))
-        #     values.append(self._grid[i, j])
-
-        # return values
-
-        # my thing
 
         values = self.column_list[i]
-        return values 
+        return values
 
     def block_values(self, i: int) -> Iterable[int]:
         """
@@ -147,53 +120,31 @@ class Sudoku:
         3 4 5
         6 7 8
         """
-        values = []
 
-        x_start = (i % 3) * 3
-        y_start = (i // 3) * 3
-
-        for x in range(x_start, x_start + 3):
-            for y in range(y_start, y_start + 3):
-                # values.append(self.value_at(x, y))
-                values.append(self._grid[x, y])
-
-        # my thing
-        # values = self.block_list[i]
-        # return values
+        values = self.block_list[i]
         return values
-        
+
     def is_solved(self) -> bool:
         """
         Returns True if and only if all rows, columns and blocks contain
         only the numbers 1 through 9. False otherwise.
         """
-        # values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        zero = 0
 
+        zero = 0
         result = True
 
         for i in range(9):
-            # for value in values:
-            #     if value not in self.column_values(i):
-            #         result = False
-            #         return result
-                if zero in self.column_values(i):
-                    result = False
-                    return result
+            if zero in self.column_values(i):
+                result = False
+                return result
 
-                # if value not in self.row_values(i):
-                #     result = False
-                #     return result
-                if zero in self.row_values(i):
-                    result = False
-                    return result 
+            if zero in self.row_values(i):
+                result = False
+                return result
 
-                # if value not in self.block_values(i):
-                #     result = False
-                #     return result
-                if zero in self.row_values(i):
-                    result = False
-                    return result 
+            if zero in self.row_values(i):
+                result = False
+                return result
 
         return result
 
